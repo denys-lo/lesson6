2ef7875260dfbd054272e2aa13edd061e10c11a5
denys-lo <div.p.div@gmail.com>
2022-12-14 23:05:24 +0100

    HW7. User portrait

--- homework7/scripts.js
+++ homework7/scripts.js
@@ -1,51 +1,51 @@
-// let getUserCity = ''
-// let getFavoriteSport = ''
-// let getYearOfBirth = prompt('Provide your year of birth:');
-//
-// if (getYearOfBirth === null) {
-//     alert('Too bad, you didn\'t want to enter your year of birth.');
-// } else {
-//     getUserCity = prompt('Provide city where you live:');
-//     if (getUserCity === null) {
-//         alert('Too bad, you didn\'t want to enter your city where you live.');
-//     } else {
-//         getFavoriteSport = prompt('Provide your favorite sport:');
-//         if (getFavoriteSport === null) {
-//             alert('Too bad, you didn\'t want to enter your favorite sport.');
-//         }
-//     }
-// }
-//
-// let userPlace = '';
-// if (getUserCity === 'Kyiv' || getUserCity === 'Washington' || getUserCity === 'London') {
-//     userPlace = 'You live in the capital in';
-// } else {
-//     userPlace = 'You live in the city';
-// }
-//
-// let userSport = 'Cool! Do you want to become ';
-// switch (getFavoriteSport) {
-//     case 'Football':
-//     case 'football':
-//         userSport += 'Messi';
-//         break;
-//     case 'Basketball':
-//     case 'basketball':
-//         userSport += 'Jordan';
-//         break;
-//     case 'Box':
-//     case 'box':
-//         userSport += 'Usyk';
-//         break;
-//     default:
-//         userSport += 'the best player';
-// }
-//
-// const result = `Your year of birth: ${getYearOfBirth}
-// ${userPlace} ${getUserCity}
-// Your favorite sport is ${getFavoriteSport}. ${userSport}?`;
-//
-//
-// if (getYearOfBirth !== null && getUserCity !== null && getFavoriteSport !== null) {
-//     alert(result)
-// }
+let getUserCity = ''
+let getFavoriteSport = ''
+let getYearOfBirth = prompt('Provide your year of birth:');
+
+if (getYearOfBirth === null) {
+    alert('Too bad, you didn\'t want to enter your year of birth.');
+} else {
+    getUserCity = prompt('Provide city where you live:');
+    if (getUserCity === null) {
+        alert('Too bad, you didn\'t want to enter your city where you live.');
+    } else {
+        getFavoriteSport = prompt('Provide your favorite sport:');
+        if (getFavoriteSport === null) {
+            alert('Too bad, you didn\'t want to enter your favorite sport.');
+        }
+    }
+}
+
+let userPlace = '';
+if (getUserCity === 'Kyiv' || getUserCity === 'Washington' || getUserCity === 'London') {
+    userPlace = 'You live in the capital in';
+} else {
+    userPlace = 'You live in the city';
+}
+
+let userSport = 'Cool! Do you want to become ';
+switch (getFavoriteSport) {
+    case 'Football':
+    case 'football':
+        userSport += 'Messi';
+        break;
+    case 'Basketball':
+    case 'basketball':
+        userSport += 'Jordan';
+        break;
+    case 'Box':
+    case 'box':
+        userSport += 'Usyk';
+        break;
+    default:
+        userSport += 'the best player';
+}
+
+const result = `Your year of birth: ${getYearOfBirth}
+${userPlace} ${getUserCity}
+Your favorite sport is ${getFavoriteSport}. ${userSport}?`;
+
+
+if (getYearOfBirth !== null && getUserCity !== null && getFavoriteSport !== null) {
+    alert(result)
+}
