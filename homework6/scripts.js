601c63b9003634090b02fde9378ba10a1e575763
denys-lo <div.p.div@gmail.com>
2022-12-14 23:03:03 +0100

    HW6. Average value

---
+++ homework6/scripts.js
@@ -0,0 +1,4 @@
+const getFirstNumber = +prompt('Provide first number', '0');
+const getSecondNumber = +prompt('Provide second number', '0');
+const getThirdNumber = +prompt('Provide third number', '0');
+alert((getFirstNumber + getSecondNumber + getThirdNumber) / 3);
