eb59c065a96ac53ef81fcb3dcab184c2276d3482
denys-lo <div.p.div@gmail.com>
2022-12-14 23:05:24 +0100

    HW7. User portrait

--- homework7/scripts.js
+++ homework7/scripts.js
@@ -12,40 +12,39 @@ if (getYearOfBirth === null) {
         getFavoriteSport = prompt('Provide your favorite sport:');
         if (getFavoriteSport === null) {
             alert('Too bad, you didn\'t want to enter your favorite sport.');
-        }
-    }
-}
+        } else {
+            let userPlace = '';
+            if (getUserCity === 'Kyiv' || getUserCity === 'Washington' || getUserCity === 'London') {
+                userPlace = 'You live in the capital in';
+            } else {
+                userPlace = 'You live in the city';
+            }
 
-let userPlace = '';
-if (getUserCity === 'Kyiv' || getUserCity === 'Washington' || getUserCity === 'London') {
-    userPlace = 'You live in the capital in';
-} else {
-    userPlace = 'You live in the city';
-}
+            let userSport = 'Cool! Do you want to become ';
+            switch (getFavoriteSport) {
+                case 'Football':
+                case 'football':
+                    userSport += 'Messi';
+                    break;
+                case 'Basketball':
+                case 'basketball':
+                    userSport += 'Jordan';
+                    break;
+                case 'Box':
+                case 'box':
+                    userSport += 'Usyk';
+                    break;
+                default:
+                    userSport += 'the best player';
+            }
 
-let userSport = 'Cool! Do you want to become ';
-switch (getFavoriteSport) {
-    case 'Football':
-    case 'football':
-        userSport += 'Messi';
-        break;
-    case 'Basketball':
-    case 'basketball':
-        userSport += 'Jordan';
-        break;
-    case 'Box':
-    case 'box':
-        userSport += 'Usyk';
-        break;
-    default:
-        userSport += 'the best player';
-}
-
-const result = `Your year of birth: ${getYearOfBirth}
+            const result = `Your year of birth: ${getYearOfBirth}
 ${userPlace} ${getUserCity}
 Your favorite sport is ${getFavoriteSport}. ${userSport}?`;
 
-
-if (getYearOfBirth !== null && getUserCity !== null && getFavoriteSport !== null) {
-    alert(result)
+            if (getYearOfBirth !== null && getUserCity !== null) {
+                alert(result)
+            }
+        }
+    }
 }
