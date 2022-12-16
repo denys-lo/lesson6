7a8eace526e0c22fa70d41550f87b98b2da3964a
denys-lo <div.p.div@gmail.com>
2022-12-16 19:46:56 +0100

    HW8. switch...case - fixed

--- homework8/scripts.js
+++ homework8/scripts.js
@@ -1,7 +1,8 @@
 let numOrStr = prompt('input number or string');
-console.log(numOrStr)
+numOrStr = (numOrStr !== null) ? numOrStr.trim(): numOrStr;
+console.log(numOrStr);
 
-switch (numOrStr.trim()) {
+switch (numOrStr) {
     case null:
         console.log('ви скасували');
         break;
